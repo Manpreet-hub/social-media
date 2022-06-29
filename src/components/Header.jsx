16b31209a51b4581dd3060e2cb695cd7047f0694
Manpreet <manpreetisar@gmail.com>
2022-06-30 01:48:24 +0530

    added logout button in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
 import LogoutIcon from "@mui/icons-material/Logout";
+import { useSelector, useDispatch } from "react-redux";
+import { logoutUser } from "../features";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const { authToken } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between p-4 font-bold shadow-lg text-2xl mb-8 sticky top-0 bg-gray-50 z-50">
       <h1 className="">Connect</h1>
-      <button>
-        <LogoutIcon />
-      </button>
+      {authToken ? (
+        <button
+          onClick={() => {
+            dispatch(logoutUser());
+            navigate("/");
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <button>Login</button>
+      )}
     </div>
   );
 };
